d5e79fdb31b1073629164956903f806465ac4632
mikeyh-aexp <mike.harrison@aexp.com>
2019-11-28 15:34:51 +0000

    Further changes

--- src/com/company/Box.java
+++ src/com/company/Box.java
@@ -1,4 +1,18 @@
 package com.company;
 
-public class Box {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T> implements Countable {
+
+    private List<T> listOfThingsInABox = new ArrayList<>();
+
+    public void addThingsToBox(T thing) {
+        listOfThingsInABox.add(thing);
+    }
+
+    @Override
+    public int getCount() {
+        return listOfThingsInABox.size();
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,23 @@ public class Main {
     public static void main(String[] args) {
 
         Counter<Apple> appleCounter = new Counter<>();
-//        Counter<Box> boxCounter = new Counter<>();
+        Box<Object> box = new Box<>();
 
+        appleCounter.add( new Apple() );
+        appleCounter.add( new Apple() );
+        appleCounter.add( new Apple() );
 
-        appleCounter.add( new Apple() );
-        appleCounter.add( new Apple() );
-        appleCounter.add( new Apple() );
+        box.addThingsToBox( new Apple() );
+        box.addThingsToBox( "String" );
+        box.addThingsToBox( 34 );
 
         System.out.println( appleCounter.getCount() );
+        System.out.println( box.getCount() );
 
+    }
+
+    private static String  changeString(String str) {
+        str += "fff";
+        return str;
     }
 }
